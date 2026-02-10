5e37d9a2ae02ca90e62fc35d939ed1166b97621b
ruoruoliu <liuyujia.victor@gmail.com>
2026-02-10 16:16:12 +0800

    update hugo-blog

--- searchindex.en.js
+++ searchindex.en.js
@@ -106,6 +106,16 @@ var relearn_searchindex = [
     "uri": "/hugo-blog/blogs/%E5%A4%A7%E6%A8%A1%E5%9E%8B%E8%A7%82%E7%82%B9andrej-karpathy%E5%92%8Crichard-sutton/index.html"
   },
   {
+    "breadcrumb": "Ruoruoliu 2.0 \u003e Weeklies",
+    "content": "总结 跟进大模型强化学习技术 跟进大模型强化学习技术 LLM训练技术学习手册 知识 DAPO 通过对gradient的clip的上下限解耦，防止模型探索不够的问题 通过只采样reward不都等于0或1的batch，来提升训练效率，防止噪声引入的波动 通过缓冲区软长度惩罚，配合token-level的loss，提升模型对过长序列和错误的区分 GSPO 将策略梯度转化为序列级，使之与reward粒度对齐 通过几何平均的形式避免数值溢出，并对长度归一化 解决MoE模型需要Routing Replay的复杂度问题 待办 Agent框架学习",
+    "description": "总结 跟进大模型强化学习技术 跟进大模型强化学习技术 LLM训练技术学习手册 知识 DAPO 通过对gradient的clip的上下限解耦，防止模型探索不够的问题 通过只采样reward不都等于0或1的batch，来提升训练效率，防止噪声引入的波动 通过缓冲区软长度惩罚，配合token-level的loss，提升模型对过长序列和错误的区分 GSPO 将策略梯度转化为序列级，使之与reward粒度对齐 通过几何平均的形式避免数值溢出，并对长度归一化 解决MoE模型需要Routing Replay的复杂度问题 待办 Agent框架学习",
+    "tags": [
+      "周记"
+    ],
+    "title": "Week13 大模型强化学习技术跟进",
+    "uri": "/hugo-blog/weekly/week13/index.html"
+  },
+  {
     "breadcrumb": "Ruoruoliu 2.0 \u003e Blogs",
     "content": "经典结构 参考链接：\nAttention Is All You Need 进展 注意力机制 MHA、MQA、GQA MHA（multi-head attention）：多头注意力，每个head维护自己的KV，在KV-Cache中占用显存很大 MQA（multi-query attention）：所有head共享KV，只有Q会被切分为多份，KV-Cache显存占用减小head-size倍 GQA（Grouped-Query Attention）：在MHA和MQA（效果和缓存）之间折中，KV分为若干份 参考链接：\nFast Transformer Decoding: One Write-Head is All You Need GQA: Training Generalized Multi-Query Transformer Models from Multi-Head Checkpoints # 理解Attention:从起源到MHA,MQA和GQA MLA KV是从输入 $x$ 线性变换得到的（先得到 $c$， 再得到 $k$ 和 $v$），如果KV-Cache里面只存 $c$（对 $x$ 进行压缩），就可以实现所有头共享（类似MQA，甚至更小）的显存占用： $$q_t^{(s)} k_i^{(s)\\top} = \\boldsymbol{x}_t \\left( W_q^{(s)} W_k^{(s)\\top} \\right) \\mathbf{c}_i^\\top$$ 在推理时，不用从 $c$ 还原 $k$ 和 $v$（会显存爆炸），而是直接对 $q$ 进行映射，然后直接与 $c$ 做乘法： $$\\text{Score} = (q_t^{(s)} W_{uk}^{(s)\\top}) \\cdot c_i^\\top$$ MLA下如何支持RoPE位置编码？ 参考链接：\n缓存与效果的极限拉扯：从MHA、MQA、GQA到MLA 位置编码 原始的位置编码分为：\n绝对位置编码：通过向量表示每个位置，但只能表示有限个（512个），且没有相对位置的概念 sinusoidal编码： 通过向量中每两位表示一个波长的绝对位置$$\\begin{aligned} PE_{(pos, 2i)} = \\sin(pos / 10000^{2i / d_{model}}) \\ PE_{(pos, 2i+1)} = \\cos(pos / 10000^{2i / d_{model}})\\end{aligned}$$ 再利用sin和cos的性质巧妙表示相对位置关系 $$\\begin{bmatrix} \\sin(\\omega_i(pos+k)) \\ \\cos(\\omega_i(pos+k)) \\end{bmatrix} = \\begin{bmatrix} \\cos(\\omega_i k) \u0026 \\sin(\\omega_i k) \\ -\\sin(\\omega_i k) \u0026 \\cos(\\omega_i k) \\end{bmatrix} \\begin{bmatrix} \\sin(\\omega_i pos) \\ \\cos(\\omega_i pos) \\end{bmatrix}$$ 对于远大于训练预料的长文本，编码的组合还是模型没有见过的，由于编码加在词向量上，KQV做点积的时候，score会发生分布偏移 RoPE RoPE的表示：\n和sinusoidal类似，把位置编码两位一组，每两位表示同一个旋转角度 对KV进行位置编码的旋转（V不参与），在KV进行点积的时候，相对位置的差异可以保持 $$f_{{q,k}}(\\mathbf{x}m, m) = \\begin{pmatrix} \\cos m\\theta \u0026 -\\sin m\\theta \\ \\sin m\\theta \u0026 \\cos m\\theta \\end{pmatrix} \\begin{pmatrix} W{{q,k}}^{(11)} \u0026 W_{{q,k}}^{(12)} \\ W_{{q,k}}^{(21)} \u0026 W_{{q,k}}^{(22)} \\end{pmatrix} \\begin{pmatrix} x_m^{(1)} \\ x_m^{(2)} \\end{pmatrix}$$ RoPE通过$W_q$ 和 $W_k$ 这两个参数矩阵学习距离的含义：\n特征解耦：模型发现，如果把某些需要考虑近距离关系的语法特征投影到旋转频率高的维度，那么点积结果就会对位置变化非常敏感 长程依赖：如果把需要长距离关联的语义特征投影到旋转频率低的维度，那么即便 $m$ 和 $n$ 相距很远，旋转后的向量依然能保持较高的相似度 相对位置的含义：由于点积结果严格遵循 $\\cos((m-n)\\theta)$，模型在训练过程中会意识到：“当这个维度的点积达到某个值时，意味着这两个词距离是 $k$“ RoPE对于长距离间的点积，距离越长，结果越小： 参考链接：\nROFORMER: ENHANCED TRANSFORMER WITH ROTARY POSITION EMBEDDING 归一化 Pre-Norm RMSNorm 网络结构 MoE Linear Transformers",
     "description": "经典结构 参考链接：\nAttention Is All You Need 进展 注意力机制 MHA、MQA、GQA MHA（multi-head attention）：多头注意力，每个head维护自己的KV，在KV-Cache中占用显存很大 MQA（multi-query attention）：所有head共享KV，只有Q会被切分为多份，KV-Cache显存占用减小head-size倍 GQA（Grouped-Query Attention）：在MHA和MQA（效果和缓存）之间折中，KV分为若干份 参考链接：",
@@ -576,16 +586,6 @@ var relearn_searchindex = [
     ],
     "title": "Week2 个人博客搭建",
     "uri": "/hugo-blog/weekly/week2/index.html"
-  },
-  {
-    "breadcrumb": "Ruoruoliu 2.0 \u003e Weeklies",
-    "content": "总结 跟进大模型强化学习技术 跟进大模型强化学习技术 LLM训练技术学习手册 知识 DAPO 通过对gradient的clip的上下限解耦，防止模型探索不够的问题 通过只采样reward不都等于0或1的batch，来提升训练效率，防止噪声引入的波动 通过缓冲区软长度惩罚，配合token-level的loss，提升模型对过长序列和错误的区分 GSPO 将策略梯度转化为序列级，使之与reward粒度对齐 通过几何平均的形式避免数值溢出，并对长度归一化 解决MoE模型需要Routing Replay的复杂度问题 待办 Agent框架学习",
-    "description": "总结 跟进大模型强化学习技术 跟进大模型强化学习技术 LLM训练技术学习手册 知识 DAPO 通过对gradient的clip的上下限解耦，防止模型探索不够的问题 通过只采样reward不都等于0或1的batch，来提升训练效率，防止噪声引入的波动 通过缓冲区软长度惩罚，配合token-level的loss，提升模型对过长序列和错误的区分 GSPO 将策略梯度转化为序列级，使之与reward粒度对齐 通过几何平均的形式避免数值溢出，并对长度归一化 解决MoE模型需要Routing Replay的复杂度问题 待办 Agent框架学习",
-    "tags": [
-      "周记"
-    ],
-    "title": "Week13 大模型强化学习技术跟进",
-    "uri": "/hugo-blog/weekly/week13/index.html"
   },
   {
     "breadcrumb": "Ruoruoliu 2.0 \u003e Weeklies",
