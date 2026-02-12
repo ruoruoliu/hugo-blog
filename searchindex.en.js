2a652ae2e77be18851750e328c5bf730ad331955
ruoruoliu <liuyujia.victor@gmail.com>
2026-02-12 11:26:25 +0800

    update hugo-blog

--- searchindex.en.js
+++ searchindex.en.js
@@ -69,8 +69,8 @@ var relearn_searchindex = [
   },
   {
     "breadcrumb": "Ruoruoliu 2.0 \u003e Blogs",
-    "content": "",
-    "description": "",
+    "content": "LangGraph是一个底层的agent编排工具\nfrom langgraph.graph import StateGraph, MessagesState, START, END def mock_llm(state: MessagesState): return {\"messages\": [{\"role\": \"ai\", \"content\": \"hello world\"}]} graph = StateGraph(MessagesState) graph.add_node(mock_llm) graph.add_edge(START, \"mock_llm\") graph.add_edge(\"mock_llm\", END) graph = graph.compile() graph.invoke({\"messages\": [{\"role\": \"user\", \"content\": \"hi!\"}]}) 参考链接：\nLangChain Docs: LangGraph",
+    "description": "LangGraph是一个底层的agent编排工具\nfrom langgraph.graph import StateGraph, MessagesState, START, END def mock_llm(state: MessagesState): return {\"messages\": [{\"role\": \"ai\", \"content\": \"hello world\"}]} graph = StateGraph(MessagesState) graph.add_node(mock_llm) graph.add_edge(START, \"mock_llm\") graph.add_edge(\"mock_llm\", END) graph = graph.compile() graph.invoke({\"messages\": [{\"role\": \"user\", \"content\": \"hi!\"}]}) 参考链接：\nLangChain Docs: LangGraph",
     "tags": [
       "技术笔记"
     ],
